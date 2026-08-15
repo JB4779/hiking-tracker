676503fd2aba17e6a2eb760032340850df2ed7e4
Tom Pierce <thomas.joseph.pierce@gmail.com>
2026-08-15 12:01:52 -0700

    Added 'Log A Hike' menu option

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
 def main():
-    print("Hello from hiking-tracker!")
+    
+    hikes = []
+
+    def log_hike():
+        date = input("Date: ")
+        trail = input("Trail name: ")
+        distance = float(input("Distance (miles): "))
+        elevation_gain = int(input("Elevation gain (feet): "))
+        total_time = int(input("Total time (minutes): "))
+        pack_weight = float(input("Pack weight (lbs): "))
+
+        hike = {
+            "date": date,
+            "trail": trail,
+            "distance": distance,
+            "elevation_gain": elevation_gain,
+            "total_time": total_time,
+            "pack_weight": pack_weight,
+        }
+
+        hikes.append(hike)
+        print("Hike logged!")
+    
+    while True:
+        print("\nHIKING TRACKER")
+        print("1. Log a hike")
+        print("2. View hikes")
+        print("3. View statistics")
+        print("4. Exit")
+
+        choice = input("\nChoose an option: ")
+
+        if choice == "1":
+            log_hike()
+        elif choice == "4":
+            print("Goodbye!")
+            break   
 
 
 if __name__ == "__main__":
